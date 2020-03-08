8bc854ee6866e2bbe30556ba0114061b55808181
Michiel Schouten <michiel.schouten@tele2.nl>
2020-03-08 20:27:45 +0100

    Initial Commit

---
+++ SwiftUiTest2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiTest2
+//
+//  Created by Michiel Schouten on 08/03/2020.
+//  Copyright © 2020 Michiel Schouten. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
